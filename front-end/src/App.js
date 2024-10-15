e76f1ff4477067fc43bfc6411613ac7f1a159c8b
Adam-Bozek <adamoss12323@gmail.com>
2024-10-15 12:14:27 +0200

    small change

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,23 +5,19 @@ import Home from "./components/LandingPage";
 import MoreInfo from "./components/MoreInfo";
 import RegistrationPage from "./components/RegistrationPage";
 import AfterRegistration from "./components/AfterRegistration";
-
 import LoginPage from "./components/LoginPage";
 
 const App = () => {
   return (
-    <>
       <Routes>
         <Route path="/Home" element={<Home />} />
         <Route path="/MoreInfo" element={<MoreInfo />} />
         <Route path="/Register" element={<RegistrationPage />} />
         <Route path="/AfterRegistration" element={<AfterRegistration />} />
-        
         <Route path="/LogIn" element={<LoginPage />} />
         
         <Route path="*" element={<Navigate replace to="/Home" />} />
       </Routes>
-    </>
   );
 };
 
