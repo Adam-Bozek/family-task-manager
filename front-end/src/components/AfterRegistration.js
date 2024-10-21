b0049b51df77580843b3e2f72b2b3577bcfb9ddb
Jakub013 <jakub13h@gmail.com>
2024-10-21 14:28:07 +0200

    fix-css

--- front-end/src/components/AfterRegistration.js
+++ front-end/src/components/AfterRegistration.js
@@ -1,50 +1,51 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./css/AfterRegistrationPage.module.css"; // Import CSS module
 
 const AfterRegistration = () => {
   const [familyName, setFamilyName] = useState('');
   const [familyCode, setFamilyCode] = useState('');
 
   const handleFamilyCreation = () => {
-    console.log('Family created:', familyName);
+    console.log('Rodina vytvorená:', familyName);
     // Add functionality for family creation
   };
 
   const handleJoinFamily = () => {
-    console.log('Joining family with code:', familyCode);
+    console.log('Prihlásenie do rodiny pomocou kódu:', familyCode);
     // Add functionality for joining a family
   };
 
   return (
-    <div className="after-registration-container">
-      <h1 className="title">A je to!</h1>
-      <p className="description">
-        Ste prihlásený ale prvým krokom je vytvorenie si rodiny alebo
-        prihlásenie sa do už existujúcej
+    <div className={styles.afterRegistrationContainer}>
+      <h1 className={styles.title}>A je to!</h1>
+      <p className={styles.description}>
+        Ste prihlásený, ale prvým krokom je vytvorenie si rodiny alebo prihlásenie sa do už existujúcej
       </p>
 
-      <div className="form-container">
-        <div className="join-family-section">
+      <div className={styles.formContainer}>
+        <div className={styles.joinFamilySection}>
           <input
             type="text"
             placeholder="Kód"
             value={familyCode}
             onChange={(e) => setFamilyCode(e.target.value)}
-            className="input-field"
+            className={styles.inputField}
           />
-          <button className="action-button" onClick={handleJoinFamily}>
+          <button className={styles.actionButton} onClick={handleJoinFamily}>
             Pridať sa k rodine
           </button>
         </div>
 
-        <div className="create-family-section">
+        <div className={styles.createFamilySection}>
           <input
             type="text"
             placeholder="Názov rodiny"
             value={familyName}
             onChange={(e) => setFamilyName(e.target.value)}
-            className="input-field"
+            className={styles.inputField}
           />
-          <button className="action-button" onClick={handleFamilyCreation}>
+          <button className={styles.actionButton} onClick={handleFamilyCreation}>
             Vytvoriť rodinu
           </button>
         </div>
