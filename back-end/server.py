9bb6078425bc6e0d38078115c17b54c5581ab9ef
Igor-Gombala <igor.gombala0123@gmail.com>
2024-10-22 13:19:02 +0200

    Returning role & Small changes

--- back-end/server.py
+++ back-end/server.py
@@ -40,14 +40,17 @@ app = Flask(__name__)
 
 @app.route('/Create_user', methods=['POST'])
 def create_user():
+    # Input
     name = request.form.get('name')
     surname = request.form.get('surname')
     email = request.form.get('email')
     password = request.form.get('password')
 
+    # SQL query
     syntax = "INSERT INTO uzivatel (meno, priezvisko, email, heslo) VALUES (%s, %s, %s, %s);"
     result = connectiondb(syntax, (name, surname, email, password))
 
+    # Return
     if result:
         return jsonify({"message": "Používateľ vytvorený."}), 201 # Created
     elif result == None:
@@ -58,33 +61,55 @@ def create_user():
 
 @app.route('/Check_user_exist', methods=['POST'])
 def check_user_exist():
+    # Input
     email = request.form.get('email')
     
+    # SQL query
     syntax = "SELECT * FROM uzivatel WHERE email = %s;"
     result = connectiondb(syntax, (email,))
     
+    print(result)
+
+    # Return
     if result:
         return jsonify({"message": "Používateľ existuje."}), 200 # OK
-    elif result == None:
-        return jsonify({"message": "Používateľ neexistuje."}), 204 # No Content
+    elif result == []:
+        return jsonify({"message": "Používateľ neexistuje."}), 406 # Not Acceptable
     else:
         return jsonify({"error": "Nastala chyba!!!"}), 404 #Not Found
     
 
 @app.route('/Login', methods=['POST'])
 def login():
+    # Input
     email = request.form.get('email')
     password = request.form.get('password')
 
+    # SQL query
     syntax = "SELECT * FROM uzivatel WHERE email = %s AND heslo = %s;"
     result = connectiondb(syntax, (email, password))
     
+    # Return Login
     if result:
-        return jsonify({"message": "Prihlásenie úspešné."}), 202 # Accepted
-    elif result == None:
+        # Get ID from result
+        id = result[0]
+        syntax1 = "SELECT rola FROM clen WHERE ID = %s;"
+        result1 = connectiondb(syntax1, (id))
+
+        print(result1)
+        
+        # Return Role
+        if result1:
+            return jsonify({"message": f"Prihlásenie úspešné. {result1[3]}"}), 202 # Accepted
+        elif result1 == None:
+            return jsonify({"message": "Prihlásenie úspešné. AfterReg"}), 202 # Accepted
+        else:
+            return jsonify({"error": "Nastala chyba pri Role!!!"}), 404 #Not Found
+        
+    elif result == []:
         return jsonify({"message": "Prihlásenie neúspešné."}), 406 # Not Acceptable
     else:
-        return jsonify({"error": "Nastala chyba!!!"}), 404 #Not Found
+        return jsonify({"error": "Nastala chyba pri Login!!!"}), 404 #Not Found
 
 if __name__ == '__main__':
     app.run(debug=True)
