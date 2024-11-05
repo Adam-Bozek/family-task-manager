e16d3e9b77bfc8f7ce600872d7c36915c45ca210
Adam-Bozek <adamoss12323@gmail.com>
2024-11-05 12:38:21 +0100

    code formatting changes in main

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,18 +2,24 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import ProtectedRoute from "./components/utilities/ProtectedRoute";
+
+// Public routes
 import Home from "./components/LandingPage";
 import MoreInfo from "./components/MoreInfo";
-import RegistrationPage from "./components/RegistrationPage";
-import AfterRegistration from "./components/AfterRegistration";
 import LoginPage from "./components/LoginPage";
+import RegistrationPage from "./components/RegistrationPage";
+
+// Protected routes (only accessible after login)
+import AfterRegistration from "./components/AfterRegistration";
+
+import ParentDashboardTasks from "./components/ParentDashboardTasks";
+import ParentDashboardRewards from "./components/ParentDashboardRewards";
+import ParentSettings from "./components/ParentSettings";
 import ParentAddTask from "./components/ParentAddTask";
-import ParentSettings from "./components/ParrentSettings";
 
-import TEMPLATE from "./components/TEMPLATE"; // This is a placeholder for your template component. Replace it with your actual component.
-
-import ProtectedRoute from "./components/utilities/ProtectedRoute"; // Import the ProtectedRoute
-
+// Template component
+import TEMPLATE from "./components/TEMPLATE";
 
 const App = () => {
 	return (
@@ -23,19 +29,23 @@ const App = () => {
 			<Route path="/MoreInfo" element={<MoreInfo />} />
 			<Route path="/Register" element={<RegistrationPage />} />
 			<Route path="/LogIn" element={<LoginPage />} />
-			<Route path="/TEST" element={<ParentSettings />} />
-			<Route path="/ParentAddTask" element={<ParentAddTask />} />
-			
+
+			{/* Testing routes */}
+			<Route path="/test_parent_dashboard_tasks" element={<ParentDashboardTasks />} />
+			<Route path="/test_parent_dashboard_rewards" element={<ParentDashboardRewards />} />
+			<Route path="/test_parent_settings" element={<ParentSettings />} />
+			<Route path="/test_parent_add_task" element={<ParentAddTask />} />
+			<Route path="/test_template" element={<TEMPLATE />} />
 
 			{/* Routes only accessible after registration */}
-			<Route path="/AfterRegistration" element={<ProtectedRoute element={<AfterRegistration />} allowedRoles={["after-reg"]} />} /> {/*Maybe hash this role or use soe string that seems hashed*/}
-
-			{/* Routes only for parents */}
+			<Route path="/AfterRegistration" element={<ProtectedRoute element={<AfterRegistration />} allowedRoles={["after-reg"]} />} />{" "}
+			
+			{/* TODO: Implement this: Routes only for parents */}
 			<Route path="/MoreInfo" element={<ProtectedRoute element={<MoreInfo />} allowedRoles={["parent"]} />} />
-
-			{/* Routes only for kids */}
+			
+			{/* TODO: Implement this: Routes only for kids */}
 			<Route path="/Login" element={<ProtectedRoute element={<LoginPage />} allowedRoles={["kid"]} />} />
-
+			
 			{/* Catch-all redirect */}
 			<Route path="*" element={<Navigate replace to="/Home" />} />
 		</Routes>
